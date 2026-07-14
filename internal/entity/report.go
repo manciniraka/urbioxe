ffcaee103ad150117dc97f9769153ebe30b29a26
nuninfarid <nunin.fzu@gmail.com>
2026-07-14 22:51:32 +0700

    chore: add needed entity & staff repository

--- internal/entity/report.go
+++ internal/entity/report.go
@@ -4,23 +4,6 @@ import (
 	"time"
 )
 
-type ReportStatus string
-type ReportPriority string
-
-const (
-	StatusPending    ReportStatus = "pending"
-	StatusVerified   ReportStatus = "verified"
-	StatusAssigned   ReportStatus = "assigned"
-	StatusInProgress ReportStatus = "in_progress"
-	StatusResolved   ReportStatus = "resolved"
-	StatusRejected   ReportStatus = "rejected"
-
-	PriorityLow      ReportPriority = "low"
-	PriorityMedium   ReportPriority = "medium"
-	PriorityHigh     ReportPriority = "high"
-	PriorityCritical ReportPriority = "critical"
-)
-
 type Report struct {
 	ID                 uint           `gorm:"primaryKey;autoIncrement"`
 	UserID             uint           `gorm:"not null"`
@@ -43,15 +26,4 @@ type Report struct {
 	District    *District          `gorm:"foreignKey:IncidentDistrictID;constraint:OnDelete:RESTRICT"`
 	Attachments []ReportAttachment `gorm:"foreignKey:ReportID;constraint:OnDelete:CASCADE"`
 	Histories   []ReportHistory    `gorm:"foreignKey:ReportID;constraint:OnDelete:CASCADE"`
-}
-
-type Category struct {
-	ID           uint   `gorm:"primaryKey;autoIncrement"`
-	DepartmentID uint   `gorm:"not null"`
-	Name         string `gorm:"type:varchar(100);not null"`
-}
-
-type District struct {
-	ID   uint   `gorm:"primaryKey;autoIncrement"`
-	Name string `gorm:"type:varchar(100);not null"`
 }
