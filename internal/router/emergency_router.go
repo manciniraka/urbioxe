a29db8c7e55552dde88ab5fb160c38515af9c921
imanuelss <imanuels1812@gmail.com>
2026-07-15 11:24:58 +0700

    feat: emergency contact management

--- internal/router/emergency_router.go
+++ internal/router/emergency_router.go
@@ -2,6 +2,9 @@ package router
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/manciniraka/urbioxe/internal/controller"
+	"github.com/manciniraka/urbioxe/internal/repository"
+	"github.com/manciniraka/urbioxe/internal/service"
 	"gorm.io/gorm"
 )
 
@@ -12,10 +15,20 @@ func RegisterEmergencyContactRoutes(
 
 	// TODO:
 	// repository
+	emergencyRepo := repository.NewEmergencyRepository(db)
 	// service
+	emergencyService := service.NewEmergencyService(emergencyRepo)
 	// controller
+	emergencyController := controller.NewEmergencyController(emergencyService)
 
 	emergency := e.Group("/emergency-contacts")
+	// Public
+	emergency.GET("", emergencyController.GetAll)
+	emergency.GET("/:id", emergencyController.GetByID)
+
+	// Department Admin / Super Admin
+	emergency.POST("", emergencyController.Create)
+	emergency.PUT("/:id", emergencyController.Update)
 
 	_ = emergency
 	_ = db
